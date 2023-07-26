9e4b59206b8f824482395e865af7f244dbd05bfc
ijord <47769255+ijord@users.noreply.github.com>
2023-07-25 21:02:28 -0700

    Double Buffer DMA eTFT

--- lib/TWatch_2021_Library/src/drive/lv_port_disp.cpp
+++ lib/TWatch_2021_Library/src/drive/lv_port_disp.cpp
@@ -1,4 +1,4 @@
-//#include "Input.h"
+// #include "Input.h"
 #include "./TWatch_hal.h"
 
 #if CONFIG_TWATCH_APP_LVGL && defined(CONFIG_TWATCH_HAS_DISPLAY)
@@ -6,26 +6,22 @@
 lv_disp_drv_t *disp_drv_p;
 
 void TWatchClass::lv_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
-  TFT_eSPI *screen = (TFT_eSPI *)disp->user_data;
-  uint32_t w = (area->x2 - area->x1 + 1);
-  uint32_t h = (area->y2 - area->y1 + 1);
-  screen->startWrite();
-  screen->setAddrWindow(area->x1, area->y1, w, h);
-#if USE_TFT_DMA == 1
-  disp_drv_p = disp;
-  screen->pushPixelsDMA((uint16_t *)color_p, w * h);
-  screen->endWrite();
-  // lv_disp_flush_ready(disp);
-#else
-  screen->pushColors((uint16_t *)&color_p->full, w * h);
-  screen->endWrite();
-  lv_disp_flush_ready(disp);
-#endif
-}
+   TFT_eSPI *screen = (TFT_eSPI *)disp->user_data;
+  // if( !framebuffer_drawing )
+  //         lv_disp_flush_ready( disp_drv );
 
-#if USE_TFT_DMA == 1
-void display_send_DMA_done_cb(spi_transaction_t *trans) {
-  lv_disp_flush_ready(disp_drv_p);
+  // get buffer sizs
+  uint32_t size = (area->x2 - area->x1 + 1) * (area->y2 - area->y1 + 1);
+  /**
+   * start data trnsmission
+   * set the working window
+   * and start DMA transfer if enabled
+   * stop transmission
+   */
+  screen->endWrite();
+  screen->startWrite();
+  screen->pushImageDMA(area->x1, area->y1, (area->x2 - area->x1 + 1), (area->y2 - area->y1 + 1), (uint16_t *)color_p);
+  lv_disp_flush_ready(disp);
 }
 #endif
 
@@ -36,22 +32,23 @@ void display_wait_cb(_lv_disp_drv_t *disp_drv) {
 void TWatchClass::lv_port_disp_init(SCREEN_CLASS *scr) {
   static lv_disp_draw_buf_t draw_buf;
 
-#if USE_TFT_DMA == 1
-  tft->initDMA(true, display_send_DMA_done_cb);
-#endif
-#if DISP_BUF_FROM_PSRAM
+      tft->init();
+      tft->fillScreen( TFT_ORANGE );
+            tft->initDMA();
+
+
+            //lv_disp_buf_p = (lv_color_t*)calloc( sizeof(lv_color_t), DISP_BUF_SIZE, MALLOC_CAP_DMA);
+            //lv_disp_buf_p2 = (lv_color_t*)calloc( sizeof(lv_color_t), DISP_BUF_SIZE, MALLOC_CAP_DMA);
+
+
   if (lv_disp_buf_p == nullptr) {
-    lv_disp_buf_p = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * DISP_BUF_SIZE, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
-    lv_disp_buf_p2 = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * DISP_BUF_SIZE, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+    lv_disp_buf_p = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * DISP_BUF_SIZE, MALLOC_CAP_DMA);
+    lv_disp_buf_p2 = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * DISP_BUF_SIZE, MALLOC_CAP_DMA);
   }
-#else
-  if (lv_disp_buf_p == nullptr) {
-    lv_disp_buf_p = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * DISP_BUF_SIZE, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-    lv_disp_buf_p2 = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * DISP_BUF_SIZE, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-  }
-#endif
-  lv_disp_draw_buf_init(&draw_buf, lv_disp_buf_p, lv_disp_buf_p2, DISP_BUF_SIZE);
-  
+
+  //lv_disp_draw_buf_init(&draw_buf, lv_disp_buf_p, NULL, DISP_BUF_SIZE );
+  lv_disp_draw_buf_init(&draw_buf, lv_disp_buf_p, lv_disp_buf_p2, DISP_BUF_SIZE );
+
   static lv_disp_drv_t disp_drv;
   lv_disp_drv_init(&disp_drv);
   disp_drv.hor_res = SCREEN_WIDTH;
@@ -66,4 +63,4 @@ void TWatchClass::lv_port_disp_init(SCREEN_CLASS *scr) {
   lv_disp_drv_register(&disp_drv);
 }
 
-#endif
+
