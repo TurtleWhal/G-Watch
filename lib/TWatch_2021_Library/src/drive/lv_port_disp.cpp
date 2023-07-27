0bdd0b51810ac397b8ffb4e738480a21de4e23ad
ijord <47769255+ijord@users.noreply.github.com>
2023-07-26 17:49:46 -0700

    Clean up eTFT DMA

--- lib/TWatch_2021_Library/src/drive/lv_port_disp.cpp
+++ lib/TWatch_2021_Library/src/drive/lv_port_disp.cpp
@@ -6,7 +6,7 @@
 lv_disp_drv_t *disp_drv_p;
 
 void TWatchClass::lv_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
-   TFT_eSPI *screen = (TFT_eSPI *)disp->user_data;
+  TFT_eSPI *screen = (TFT_eSPI *)disp->user_data;
   // if( !framebuffer_drawing )
   //         lv_disp_flush_ready( disp_drv );
 
@@ -18,9 +18,9 @@ void TWatchClass::lv_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_c
    * and start DMA transfer if enabled
    * stop transmission
    */
-  screen->endWrite();
   screen->startWrite();
   screen->pushImageDMA(area->x1, area->y1, (area->x2 - area->x1 + 1), (area->y2 - area->y1 + 1), (uint16_t *)color_p);
+  screen->endWrite();
   lv_disp_flush_ready(disp);
 }
 #endif
@@ -32,22 +32,20 @@ void display_wait_cb(_lv_disp_drv_t *disp_drv) {
 void TWatchClass::lv_port_disp_init(SCREEN_CLASS *scr) {
   static lv_disp_draw_buf_t draw_buf;
 
-      tft->init();
-      tft->fillScreen( TFT_ORANGE );
-            tft->initDMA();
+  tft->init();
+  //tft->fillScreen(TFT_BLACK);
+  tft->initDMA();
 
-
-            //lv_disp_buf_p = (lv_color_t*)calloc( sizeof(lv_color_t), DISP_BUF_SIZE, MALLOC_CAP_DMA);
-            //lv_disp_buf_p2 = (lv_color_t*)calloc( sizeof(lv_color_t), DISP_BUF_SIZE, MALLOC_CAP_DMA);
-
+  // lv_disp_buf_p = (lv_color_t*)calloc( sizeof(lv_color_t), DISP_BUF_SIZE, MALLOC_CAP_DMA);
+  // lv_disp_buf_p2 = (lv_color_t*)calloc( sizeof(lv_color_t), DISP_BUF_SIZE, MALLOC_CAP_DMA);
 
   if (lv_disp_buf_p == nullptr) {
     lv_disp_buf_p = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * DISP_BUF_SIZE, MALLOC_CAP_DMA);
-    lv_disp_buf_p2 = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * DISP_BUF_SIZE, MALLOC_CAP_DMA);
+    //lv_disp_buf_p2 = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * DISP_BUF_SIZE, MALLOC_CAP_DMA);
   }
 
-  //lv_disp_draw_buf_init(&draw_buf, lv_disp_buf_p, NULL, DISP_BUF_SIZE );
-  lv_disp_draw_buf_init(&draw_buf, lv_disp_buf_p, lv_disp_buf_p2, DISP_BUF_SIZE );
+   lv_disp_draw_buf_init(&draw_buf, lv_disp_buf_p, NULL, DISP_BUF_SIZE );
+  // lv_disp_draw_buf_init(&draw_buf, lv_disp_buf_p, lv_disp_buf_p2, DISP_BUF_SIZE);
 
   static lv_disp_drv_t disp_drv;
   lv_disp_drv_init(&disp_drv);
@@ -62,5 +60,3 @@ void TWatchClass::lv_port_disp_init(SCREEN_CLASS *scr) {
 
   lv_disp_drv_register(&disp_drv);
 }
-
-
