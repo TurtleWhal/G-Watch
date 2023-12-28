757f10453cb3e3fb9dcc386fa6c5ce1ebd934dc8
TurtleWhal <garrettrjordan@gmail.com>
2023-12-28 14:56:23 -0800

    Added look at watch detection

--- lib/TWatch_2021_Library/src/drive/bma423.cpp
+++ lib/TWatch_2021_Library/src/drive/bma423.cpp
@@ -10,54 +10,46 @@
 #define BMA4_INT_MAP BMA4_INTR2_MAP
 #endif
 
-static float lsb_to_ms2(int16_t val, float g_range, uint8_t bit_width)
-{
+static float lsb_to_ms2(int16_t val, float g_range, uint8_t bit_width) {
   float half_scale = (float)(1 << bit_width) / 2.0f;
 
   return GRAVITY_EARTH * val * g_range / half_scale;
 }
 
-int8_t readRegister(uint8_t reg, uint8_t *data, uint32_t len, void *intf_ptr)
-{
+int8_t readRegister(uint8_t reg, uint8_t *data, uint32_t len, void *intf_ptr) {
   Wire.beginTransmission(BMA4_I2C_ADDR_PRIMARY);
   Wire.write(reg);
   Wire.endTransmission();
   Wire.requestFrom((uint8_t)BMA4_I2C_ADDR_PRIMARY, (uint8_t)len);
   uint8_t i = 0;
-  while (Wire.available())
-  {
+  while (Wire.available()) {
     data[i++] = Wire.read();
   }
   return 0; // Pass
 }
 
-int8_t writeRegister(uint8_t reg, const uint8_t *data, uint32_t len, void *intf_ptr)
-{
+int8_t writeRegister(uint8_t reg, const uint8_t *data, uint32_t len, void *intf_ptr) {
   Wire.beginTransmission(BMA4_I2C_ADDR_PRIMARY);
   Wire.write(reg);
   Wire.write(data, len);
   return (0 != Wire.endTransmission());
 }
 
-void delay_us(uint32_t period, void *intf_ptr)
-{
+void delay_us(uint32_t period, void *intf_ptr) {
   ets_delay_us(period);
 }
 
-int8_t bma4_interface_selection(struct bma4_dev *bma, uint8_t variant)
-{
+int8_t bma4_interface_selection(struct bma4_dev *bma, uint8_t variant) {
   int8_t rslt = BMA4_OK;
   uint8_t dev_addr;
-  if (bma != NULL)
-  {
+  if (bma != NULL) {
     /* Select the interface for execution
      * For I2C : BMA4_I2C_INTF
      * For SPI : BMA4_SPI_INTF
      */
     bma->intf = BMA4_I2C_INTF;
     /* Bus configuration : I2C */
-    if (bma->intf == BMA4_I2C_INTF)
-    {
+    if (bma->intf == BMA4_I2C_INTF) {
       // printf("I2C Interface \n");
       /* To initialize the user I2C function */
       // Wire.begin(TWATCH_IICSDA, TWATCH_IICSCL);
@@ -74,53 +66,38 @@ int8_t bma4_interface_selection(struct bma4_dev *bma, uint8_t variant)
     bma->delay_us = delay_us;
     /* Configure max read/write length (in bytes) ( Supported length depends on target machine) */
     bma->read_write_len = 8;
-  }
-  else
-  {
+  } else {
     rslt = BMA4_E_NULL_PTR;
   }
 
   return rslt;
 }
 
-void bma4_error_codes_print_result(const char api_name[], uint16_t rslt)
-{
-  if (rslt != BMA4_OK)
-  {
+void bma4_error_codes_print_result(const char api_name[], uint16_t rslt) {
+  if (rslt != BMA4_OK) {
     DEBUGF("%s\t", api_name);
-    if (rslt & BMA4_E_NULL_PTR)
-    {
+    if (rslt & BMA4_E_NULL_PTR) {
       DEBUGF("Error [%d] : Null pointer\r\n", rslt);
-    }
-    else if (rslt & BMA4_E_CONFIG_STREAM_ERROR)
-    {
+    } else if (rslt & BMA4_E_CONFIG_STREAM_ERROR) {
       DEBUGF("Error [%d] : Invalid configuration stream\r\n", rslt);
-    }
-    else if (rslt & BMA4_E_SELF_TEST_FAIL)
-    {
+    } else if (rslt & BMA4_E_SELF_TEST_FAIL) {
       DEBUGF("Error [%d] : Self test failed\r\n", rslt);
-    }
-    else if (rslt & BMA4_E_INVALID_SENSOR)
-    {
+    } else if (rslt & BMA4_E_INVALID_SENSOR) {
       DEBUGF("Error [%d] : Device not found\r\n", rslt);
-    }
-    else
-    {
+    } else {
       /* For more error codes refer "*_defs.h" */
       DEBUGF("Error [%d] : Unknown error code\r\n", rslt);
     }
   }
 }
 
-void TWatchClass::bma423_interface_init()
-{
+void TWatchClass::bma423_interface_init() {
   bma423 = new bma4_dev;
   uint16_t rslt = bma4_interface_selection(bma423, BMA42X_VARIANT);
   bma4_error_codes_print_result("bma4_interface_selection status", rslt);
 }
 
-void TWatchClass::bma423_begin()
-{
+void TWatchClass::bma423_begin() {
   uint16_t rslt;
   struct bma423_axes_remap remap;
   struct bma4_accel_config accel_conf;
@@ -135,8 +112,7 @@ void TWatchClass::bma423_begin()
   else
     DEBUGLN("bma423 not find");
 
-  if (_isinited_bma423)
-  {
+  if (_isinited_bma423) {
     rslt = bma423_write_config_file(bma423);
     bma4_error_codes_print_result("bma423_write_config", rslt);
 
@@ -163,8 +139,14 @@ void TWatchClass::bma423_begin()
     bma4_error_codes_print_result("bma4_set_accel_config status", rslt);
 
     bma423_acc_feature(true);
+
     // AccSensor_Feature(BMA423_WRIST_WEAR | BMA423_SINGLE_TAP | BMA423_DOUBLE_TAP | BMA423_STEP_CNTR, true);
     // AccSensor_Feature_Int(BMA423_WRIST_WEAR_INT | BMA423_STEP_CNTR_INT | BMA423_SINGLE_TAP_INT | BMA423_DOUBLE_TAP_INT, true);
+
+    // enable the wrist tilt interrupt
+    bma423_feature(BMA423_WRIST_WEAR, true);
+    bma423_feature_int(BMA423_WRIST_WEAR_INT, true);
+
     bma423_feature(BMA423_STEP_CNTR, true);
     bma423_feature_int(BMA423_STEP_CNTR_INT, false);
 
@@ -185,11 +167,9 @@ void TWatchClass::bma423_begin()
  * BMA423_SINGLE_TAP
  * BMA423_DOUBLE_TAP
  */
-void TWatchClass::bma423_feature(uint8_t feature, bool Enable)
-{
+void TWatchClass::bma423_feature(uint8_t feature, bool Enable) {
   uint16_t rslt;
-  if (_isinited_bma423)
-  {
+  if (_isinited_bma423) {
     rslt = bma423_feature_enable(feature, Enable, bma423);
     bma4_error_codes_print_result("bma423_feature_enable status", rslt);
   }
@@ -206,25 +186,20 @@ void TWatchClass::bma423_feature(uint8_t feature, bool Enable)
 *  BMA423_NO_MOT_INT
 *  BMA423_ERROR_INT
  */
-void TWatchClass::bma423_feature_int(uint8_t feature, bool Enable)
-{
+void TWatchClass::bma423_feature_int(uint8_t feature, bool Enable) {
   uint16_t rslt;
-  if (_isinited_bma423)
-  {
+  if (_isinited_bma423) {
     rslt = bma423_map_interrupt(BMA4_INT_MAP, feature, Enable, bma423);
     bma4_error_codes_print_result("bma423_map_interrupt status", rslt);
   }
 }
 
-void TWatchClass::bma423_updata(uint32_t millis, uint32_t time_ms)
-{
+void TWatchClass::bma423_updata(uint32_t millis, uint32_t time_ms) {
   static uint32_t Millis;
   uint16_t int_status = 0;
   struct bma4_accel sens_data;
-  if (millis - Millis > time_ms)
-  {
-    if (_isinited_bma423)
-    {
+  if (millis - Millis > time_ms) {
+    if (_isinited_bma423) {
       uint16_t rslt = bma423_read_int_status(&int_status, bma423);
 
       rslt = bma423_step_counter_output(&stepCount, bma423);
@@ -239,24 +214,19 @@ void TWatchClass::bma423_updata(uint32_t millis, uint32_t time_ms)
   }
 }
 
-void TWatchClass::bma423_step_reset(void)
-{
-  if (_isinited_bma423)
-  {
+void TWatchClass::bma423_step_reset(void) {
+  if (_isinited_bma423) {
     bma423_reset_step_counter(bma423);
   }
 }
 
-void TWatchClass::bma423_acc_feature(bool Enable)
-{
-  if (_isinited_bma423)
-  {
+void TWatchClass::bma423_acc_feature(bool Enable) {
+  if (_isinited_bma423) {
     uint16_t rslt = bma4_set_accel_enable(Enable, bma423);
     bma4_error_codes_print_result("bma4_set_accel_enable status", rslt);
   }
 }
-void TWatchClass::bma423_reset(void)
-{
+void TWatchClass::bma423_reset(void) {
   bma4_soft_reset(bma423);
 }
 
@@ -276,12 +246,10 @@ uint32_t TWatchClass::bma423_get_step() { return stepCount; }
  * BMA4_KELVIN |   degrees kelvin
  *
  */
-float TWatchClass::bma423_get_temperature(uint8_t temp_unit)
-{
+float TWatchClass::bma423_get_temperature(uint8_t temp_unit) {
   int32_t get_temp = 0;
   float actual_temp = 0;
-  if (_isinited_bma423)
-  {
+  if (_isinited_bma423) {
     bma4_get_temperature(&get_temp, temp_unit, bma423);
     actual_temp = (float)get_temp / (float)BMA4_SCALE_TEMP;
   }
@@ -299,10 +267,8 @@ float TWatchClass::bma423_get_temperature(uint8_t temp_unit)
  *  BMA423_NO_MOT_INT
  *  BMA423_ERROR_INT
  */
-void TWatchClass::bma423_set_feature_event(uint8_t feature, irq_Fun_cb_t cb)
-{
-  switch (feature)
-  {
+void TWatchClass::bma423_set_feature_event(uint8_t feature, irq_Fun_cb_t cb) {
+  switch (feature) {
   case BMA423_SINGLE_TAP_INT:
     _bma_irq_cb.single_tap_cb = cb;
     break;
